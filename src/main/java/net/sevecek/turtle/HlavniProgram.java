5e07590a49a0aea71bba76b6d413c96e5f0c1c00
Josef Lukeš <josef.lukes@gmail.com>
2022-04-16 21:31:53 +0200

    Část 3 - Kompozice

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -10,18 +10,62 @@ public class HlavniProgram {
         zofka = new Turtle();
 
 
-        zofka.setSpeed(10);
+        zofka.setSpeed(100);
 
-        this.nakresliPrasatko(zofka,200,10);
+        //this.nakresliPrasatko(zofka,200,10);
 
+        //zofka.turnLeft(90);
+        //this.nakresliNUhelnik(zofka, 50,8);
 
+        //zofka.move(200);
+        //this.nakresliNUhelnik(zofka, 8,50);
+        //zofka.move(200);
+        //this.nakresliSlunicko(zofka, 20,12);
 
-
+        this.nakresliKompozici(zofka);
 
     }
+    public void nakresliKompozici(Turtle zelva) {
+        zelva.setLocation(150,50);
+        nakresliSlunicko(zelva,10,12);
+        zelva.setLocation(100,180);
+        nakresliRaduDomu(zelva,50,5,50);
+        zelva.setLocation(100,300);
+        nakresliRaduDomu(zelva,50,2,350);
+        zelva.setLocation(300,300);
+        nakresliPrasatko(zelva,50,20);
+    }
+    public void nakresliRaduDomu(Turtle zelva, int delka, int pocetDomu, int odstup) {
+        for (int i = 0; i < pocetDomu; i++) {
+            nakresliDomecek(zelva, delka);
+            zelva.turnLeft(60);
+            zelva.penUp();
+            zelva.move(odstup);
+            zelva.turnRight(90);
+            zelva.move(delka);
+            zelva.turnRight(180);
+            zelva.penDown();
+        }
+    }
+    public void nakresliNUhelnik(Turtle zelva, int delka, int pocetkroku) {
 
+        for (int i = 0; i < pocetkroku; i++) {
+            zelva.move(delka);
+            zelva.turnRight(360.0 / pocetkroku);
+        }
+    }
+    public void nakresliSlunicko(Turtle zelva, int delka, int pocetkroku) {
 
-
+        for (int i = 0; i < pocetkroku; i++) {
+            zelva.move(delka);
+            zelva.turnRight( 360.0/pocetkroku);
+            zelva.turnLeft(90);
+            zelva.move(delka);
+            zelva.turnRight(180);
+            zelva.move(delka);
+            zelva.turnLeft(90);
+        }
+    }
 
     public void nakresliPrasatko(Turtle zelva,int delkaSteny, int delkaNozicky) {
         ctverec(zelva,delkaSteny);
@@ -34,14 +78,21 @@ public class HlavniProgram {
         nozicka(zelva,delkaNozicky);
         strecha(zelva,delkaSteny);
     }
+
+    public void nakresliDomecek(Turtle zelva, int delka) {
+
+        ctverec(zelva, delka);
+        zelva.move(delka);
+        strecha(zelva, delka);
+
+    }
+
     public void ctverec(Turtle zelva, int delka) {
 
         for (int i = 0; i < 4; i++) {
             zelva.move(delka);
             zelva.turnRight(90);
-
         }
-        //zelva.move(150);
     }
 
         public void strecha(Turtle zelva, int delka) {
